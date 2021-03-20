03849ff8e4582eed0d9a5ef734354200985f1ea1
Jose Cossi <Jose Cossi@192.168.0.203>
2021-03-20 09:29:54 -0300

    Hola Mundo con Java

---
+++ src/holamundojava/HolaMundoJava.java
@@ -0,0 +1,11 @@
+
+package holamundojava;
+
+public class HolaMundoJava {
+
+    public static void main(String[] args) {
+       
+        System.out.println("Hola Mundo con Java PoloTic Misiones");
+    }
+    
+}
